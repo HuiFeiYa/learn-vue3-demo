4d73e5af1c1c356604008f0adfbd7e27150dc3ce
宋远桥 <wanxiao.guan@ideacome.com>
2020-09-25 10:49:35 +0800

    [feature] picture wall

--- src/views/greenScreen/template/exhibit/index.vue
+++ src/views/greenScreen/template/exhibit/index.vue
@@ -4,19 +4,10 @@
       <Title></Title>
     </div>
     <div class="stage">
+      <div class="picture-wall">
+        <PictureWall v-for="(item,index) in picWallList" :ind="index" :key="index" :rotate="item.rotate" :path="item.path"></PictureWall>
+      </div>
       <!-- 一组 -->
-      <div class="flex">
-        <div>
-          <img class="default" src="./images/img.jpg" alt="">
-        </div>
-        <Numberfal :isMove="isChangeNumber1"></Numberfal>
-      </div>
-      <div class="flex">
-        <div>
-          <img class="default" src="./images/img.jpg" alt="">
-        </div>
-        <Numberfal :isMove="isChangeNumber1"></Numberfal>
-      </div>
       <div class="flex">
         <div>
           <img class="default" src="./images/img.jpg" alt="">
@@ -36,15 +27,51 @@
 <script>
   import Title from './title.vue'
   import Numberfal from './numeral'
+  import PictureWall from './pictureWall'
   export default {
     name: '',
     components:{
       Title,
-      Numberfal
+      Numberfal,
+      PictureWall
     },
     data() {
       return {
-        isChangeNumber1:false
+        isChangeNumber1:false,
+        picWallList:[
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7c578cb3102b47aebe9981aac3513eb8~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '10deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c113287e50f34bcba68938557e51913f~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '0deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7c578cb3102b47aebe9981aac3513eb8~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '-15deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c113287e50f34bcba68938557e51913f~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '15deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7c578cb3102b47aebe9981aac3513eb8~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '-25deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c113287e50f34bcba68938557e51913f~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '20deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7c578cb3102b47aebe9981aac3513eb8~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '-5deg'
+          },
+          {
+            path:'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/c113287e50f34bcba68938557e51913f~tplv-k3u1fbpfcp-zoom-1.image',
+            rotate: '-15deg'
+          },
+        ]
       }
     },
     mounted() {
@@ -52,9 +79,8 @@
     },
     methods: {
       listener() {
-        console.log('1')
         document.addEventListener('scroll',event=>{
-          console.log('top',document.documentElement.scrollTop)
+          // console.log('top',document.documentElement.scrollTop)
           if(document.documentElement.scrollTop > 80) {
             this.isChangeNumber1 = true
           }
@@ -66,6 +92,12 @@
 <style>
 </style>
 <style lang='less' scoped>
+.picture-wall{
+  position: relative;
+  height: 600px;
+}
+
+
 .flex{
   display: flex;
 }
---
+++ src/views/greenScreen/template/exhibit/pictureWall.vue
@@ -0,0 +1,68 @@
+<template>
+<transition name="router-slid">
+  <div class="one-pic">
+    <div class="wrap"   :style="{
+        transform:`rotate(${rotate})`
+      }">
+      <img   v-show="isShow" :src="path" alt="">
+    </div>
+  </div>
+</transition>
+</template>
+
+<script>
+  export default {
+    name: '',
+    props:{
+      rotate:{
+        type:String,
+        default:'10deg'
+      },
+      path:{
+        type:String,
+        default:''
+      },
+      ind:{
+        type:Number,
+        default: 0
+      }
+    },
+    data() {
+      return {
+        isShow:false
+      }
+    },
+    created() {
+      setTimeout(() => {
+        this.isShow = true
+        console.log('执行')
+      }, (this.ind + 1) * 1000);
+    },
+    methods: {}
+  }
+</script>
+
+<style lang='less' scoped>
+
+.one-pic{
+  position: absolute;
+  top:100px;
+  left:50%;
+  transform: translateX(-50%);
+  text-align: center;
+  
+  .wrap{
+    border:3px solid #eee;
+    width: 600px;
+    height: 337px;
+    display: inline-block;
+  }
+  // transform: rotate(30deg);
+  img{
+    width:600px;
+    vertical-align: middle;
+    position: relative;
+    z-index: 9999;
+  }
+}
+</style>
