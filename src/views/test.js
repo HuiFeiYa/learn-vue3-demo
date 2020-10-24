9b5d077071c2a19488b30b2b8e33a753792fb1c0
宋远桥 <wanxiao.guan@ideacome.com>
2020-10-24 08:35:42 +0800

    [feature] unit2 canvas

--- src/views/test.js
+++ src/views/test.js
@@ -1,84 +1,51 @@
-// 将s[i]向上拨动一次
-const plusOne = function(s, j) {
-  const arr = s.split('')
-  if (arr[j] === '9') {
-    arr[j] = '0'
-  } else {
-    arr[j] = +arr[j] + 1 + ''
-  }
-
-  return arr.join('')
-}
-
-// 将s[i]向下拨动一次
-const minusOne = function(s, j) {
-  const arr = s.split('')
-  if (arr[j] === '0') {
-    arr[j] = '9'
-  } else {
-    arr[j] = +arr[j] - 1 + ''
-  }
-
-  return arr.join('')
-}
-
-/**
- * @param {string[]} deadends
- * @param {string} target
- * @return {number}
- */
-const openLock = function(deadends, target) {
-  // 记录需要跳过的死亡密码
-  const deads = new Set()
-  for (let i = 0; i < deadends.length; i++) {
-    deads.add(deadends[i])
-  }
-
-  // 记录已经穷举过的密码，防止重复访问
-  const visited = new Set()
-
-  // BFS核心队列
-  const q = []
-
-  // 从起点开始BFS
-  let step = 0
-  q.push('0000')
-  visited.add('0000')
-
-  while (q.length > 0) {
-    console.log('q', q)
-    const sz = q.length
-    // 将当前队列中的所有的节点向四周扩散
-    for (let i = 0; i < sz; i++) {
-      const cur = q.shift()
-
-      // 判断是否到达终点
-      if (deads.has(cur)) {
-        continue
-      }
-      if (cur === target) {
-        return step
-      }
-
-      // 将一个节点的未遍历相邻节点加入队列
-      for (let j = 0; j < 4; j++) {
-        const up = plusOne(cur, j)
-        if (!visited.has(up)) {
-          q.push(up)
-          visited.add(up)
+// 思路先找到最小值，然后找下一个最大值，不停的找出每个波段的波谷和波峰
+const maxProfit = function(prices) {
+  let min = Number.MAX_SAFE_INTEGER
+  let max = Number.MIN_SAFE_INTEGER
+  let notMin = false
+  let profit = 0
+  // 找出当前列表第一段中的波谷
+  function minNum() {
+    for (const [i, price] of prices.entries()) {
+      // 如果下一个值更小就更新到波谷值中
+      if (price <= min) {
+        min = price
+        // [5,4,3,2] 如果是倒叙的列表，这种情况应该直接返回0，股票一直再跌没有交易机会
+        if (i === prices.length - 1) {
+          prices = []
+          notMin = true
         }
-
-        const down = minusOne(cur, j)
-        if (!visited.has(down)) {
-          q.push(down)
-          visited.add(down)
-        }
+      } else {
+        // 找到波谷后将存储列表前的元素清除掉
+        prices = prices.slice(i)
+        break
       }
     }
-    step++
   }
-  return -1
+  // 找到当前阶段的波峰
+  function maxNum() {
+    for (const [i, price] of prices.entries()) {
+      // 找到波峰点
+      if (price >= max) {
+        max = price
+        // [2,3,4,5] 如果是正序，股票一直在涨
+        if (i === prices.length - 1) {
+          prices = []
+        }
+      } else {
+        prices = prices.slice(i)
+        break
+      }
+    }
+  }
+  while (prices.length > 0) {
+    // 一轮遍历周期就是找到一个波谷和一个波峰然后计算利润
+    minNum()
+    maxNum()
+    if (max === min || notMin) break
+    profit += max - min
+    min = Number.MAX_SAFE_INTEGER
+    max = Number.MIN_SAFE_INTEGER
+  }
+  return profit
 }
-
-const r = openLock(['0201', '0101', '0102', '1212', '2002'], '0202')
-console.log('r', r)
